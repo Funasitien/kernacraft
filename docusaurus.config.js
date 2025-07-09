b15500041748314f987503ff1649d26a55df1a7a
Funasitien DreamCloud <hello@funa.dev>
2025-07-09 18:00:33 +0200

    ⬆️ Mise à jour URL

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,16 +11,16 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://www.kernacraft.studio',
+  url: 'https://funa.dev/kernacraft/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/kernacraft/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   // If you use CNAME Record, you can disable this
   organizationName: 'Funasitien', // Usually your GitHub org/user name.
-  projectName: 'kernadoc', // Usually your repo name.
+  projectName: 'kernacraft', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
